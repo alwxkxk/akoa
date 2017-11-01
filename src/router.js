f37f2f22f98578168b4b5fe876b02b1546507057
生子当如哈士奇 <592364224@qq.com>
2017-11-01 14:36:17 +0800

    添加body-parse

--- src/router.js
+++ src/router.js
@@ -1,11 +1,21 @@
 let router = require('koa-better-router')().loadMethods()
-
+let body = require('koa-better-body')
 router.addRoute('GET', '/', (ctx, next) => {
   ctx.body = 'this is /index'
 })
 
-router.addRoute('GET', '/login', (ctx, next) => {
-  ctx.body = 'this is /login'
+router.post('/login',body(),function *(next) {
+  console.log(this.request.files)
+  console.log(this.request.fields)
+
+  // there's no `.body` when `multipart`,
+  // `urlencoded` or `json` request
+  console.log(this.request.body)
+  this.response.status=200
+  this.response.message='alw message??'
+  this.response.body={code:200}
+  yield next
 })
+
 
 module.exports = router
