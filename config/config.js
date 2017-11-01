f63488ea25de40a04b187efb8ce0de3c2af392fd
生子当如哈士奇 <592364224@qq.com>
2017-11-01 15:49:33 +0800

    添加配置文件

---
+++ config/config.js
@@ -0,0 +1,10 @@
+let config = {
+  mysqlConfig: {
+    host: 'gz-cdb-jptqaaf0.sql.tencentcdb.com',
+    port: '63759',
+    user: 'root',
+    password: 'geeku@2017',
+    database: 'test'
+  }
+}
+module.exports = config
