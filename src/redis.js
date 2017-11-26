69d14a1f9ed5914896091088a673d2fdee003a29
生子当如哈士奇 <592364224@qq.com>
2017-11-26 08:13:04 +0800

    增加了redis部分函数与单元测试

--- src/redis.js
+++ src/redis.js
@@ -1,10 +1,14 @@
+
 const redisConfig = require('../config/config.js').redisConfig
 const redis = require('redis')
 const bluebird = require('bluebird')
+const uti = require('./utilities.js')
+const md5 = require('md5')
 
 let client = redis.createClient(redisConfig)
 
 bluebird.promisifyAll(redis.RedisClient.prototype)
+// 到此，可以使用加Async的后缀实现promise化 client.getAsync('foo').then(function(res) {
 bluebird.promisifyAll(redis.Multi.prototype)
 
 client.on('error', function (err) {
@@ -26,6 +30,53 @@ client.on('end', function (err) {
   console.log('redis end.')
 })
 
+/**
+ * 以name创建两条缓存，分别是name->token,token->user，且会将旧token删除。
+ *
+ * @param {string} name 账号名
+ * @returns {Promise} 返回Promise对象，resolve values[token,reply,reply],reject err
+ */
+client.setToken = async function setToken (name) {
+  let now = uti.now()
+  let token = md5(name + now)
+  await client.deleteToken(name)// 将旧的token删除
+
+  return Promise.all([
+    Promise.resolve(token), // 将token传出去
+    client.hsetAsync(token, 'name', name, 'create', now),
+    client.setAsync(name, token)
+  ])
+}
+/**
+ * 删除name及其对应的token两个缓存
+ *
+ * @param {string} name 账号名
+ * @returns {Promise} 返回Promise对象，resolve {redisReply},reject err
+ */
+client.deleteToken = function deleteToken (name) {
+  return client.getAsync(name)
+  .then(token => {
+    // console.log('will delete',token)
+    return token ? client.delAsync(token) : Promise.resolve('token已失效')
+  })
+  .then(v => { return client.delAsync(name) })
+}
+/**
+ * 检验token是否有效
+ *
+ * @param {string} token
+ * @returns {Promise} 返回Promise对象，resolve 'token有效',reject err||'token无效'
+ */
+client.tokenValidate = function tokenValidate (token) {
+  return client.hexistsAsync(token, 'name')
+  .then(reply => {
+    // console.log(reply,typeof reply)   //reply是number类型
+    return reply === 1 ? Promise.resolve('token有效') : Promise.reject('token无效')
+  })
+}
+// TODO:初始化，将所有用户名保存到一个列表中
+// TODO:提供 redis检测有无用户名重名的API   nameUnique
+// TODO:设置缓存时间
 // 结束连接
 // client.quit()
 module.exports = client
--- test/redis.test.js
+++ test/redis.test.js
@@ -1,3 +1,4 @@
+
 let assert = require('assert')
 let redis = require('../src/redis.js')
 
@@ -9,7 +10,7 @@ setTimeout(function () {
         redis.quit()
       }, 1000)
     })
-    describe('redis 简单测试', function () {
+    describe('redis基本语法 简单测试', function () {
       it('SET', function (done) {
         redis.setAsync('test', '123')
         .then((reply) => {
@@ -17,6 +18,39 @@ setTimeout(function () {
           done()
         })
         .catch((err) => { done(err) })
+      })
+      it('DEL', function (done) {
+        redis.delAsync('test')
+        .then((reply) => {
+          assert.equal(reply, 1)
+          done()
+        })
+        .catch((err) => { done(err) })
+      })
+    })
+    describe('自定义函数测试setToken，tokenValidate，deleteToken', function () {
+      it('setToken会产生token(两个缓存)，tokenValidate检测token存在与否，deleteToken删除token', async function () {
+        let token
+        await redis.setToken('setTokenTest')
+        .then((replys) => {
+          token = replys[0]
+          return Promise.resolve(token)
+        })
+        .then(token => {
+          // console.log(token)
+          return redis.tokenValidate(token)
+        })
+        .then(v => {
+          // console.log(v)
+          return redis.getAsync('setTokenTest')
+          .then(v => {
+            assert.equal(v, token, 'setTokenTest的值与所设token不同')
+          })
+        })
+        .then(v => {
+          return redis.deleteToken('setTokenTest')
+        })
+        .catch((err) => { assert.fail(err) })
       })
     })
   })
