b6a082223ccf8c27899caee405582dd918eb9563
生子当如哈士奇 <592364224@qq.com>
2017-12-01 20:55:11 +0800

    使用配置文件error-code.js

--- src/api.js
+++ src/api.js
@@ -17,14 +17,16 @@ router.post('/test', body(), function * (next) {
     files: this.request.files || null,
     body: this.request.body || null
   }
-  console.log(data)
-  this.cookies.set('cookieTest', 'test')// 测试cookies
+  // console.log(data)
+  // this.cookies.set('cookieTest', 'test')// 测试cookies
+  console.log(this.cookies.get('token'))
+  console.log(this.cookies)
   this.response.type = 'json'  // 这里的this 等效于ctx
-  this.response.body = uti.httpResponse(0, '测试专用接口', data)
+  this.response.body = uti.httpResponse(0, data)
   yield next
 })
 
-// POST /api/user 注册用户
+// POST /api/user 注册账号
 router.post('/user', body(), async function (ctx, next) {
   // 参数检查
   let post = ctx.request.fields
@@ -37,11 +39,11 @@ router.post('/user', body(), async function (ctx, next) {
 
   if (result.error) {
     let details = { details: _.map(result.error.details, 'message') }
-    ctx.response.body = uti.httpResponse(1, '注册账号失败', details)
+    ctx.response.body = uti.httpResponse(1001, details)
   } else {
     await User.register(post.name, post.password)
-      .then(() => { ctx.response.body = uti.httpResponse(0, '注册账号成功') })
-      .catch((err) => { ctx.response.body = uti.httpResponse(1, '注册账号失败', { detail: err }) })
+      .then(() => { ctx.response.body = uti.httpResponse(0) })
+      .catch((err) => { ctx.response.body = uti.httpResponse(1001, { detail: err }) })
   }
   return next()
 },
@@ -49,7 +51,7 @@ router.post('/user', body(), async function (ctx, next) {
     yield next
   })
 
-// POST /api/session  用户登陆成功 创建新的会话 返回token并设置为cookie
+// POST /api/session  账号登陆成功 创建新的会话 返回token并设置为cookie
 router.post('/session', body(), async function (ctx, next) {
  // 参数检查
   let data = ctx.request.fields
@@ -63,17 +65,17 @@ router.post('/session', body(), async function (ctx, next) {
   if (result.error) {
    // 取得参数有误的所有messages
     let details = { details: _.map(result.error.details, 'message') }
-    ctx.response.body = uti.httpResponse(1, '用户登陆失败,参数有误', details)
+    ctx.response.body = uti.httpResponse(1001, details)
     return next()
   }
-   // 用户登陆
+   // 账号登陆
   await User.login(data.name, data.password)
     .then((token) => {
       ctx.cookies.set('token', token)
-      ctx.response.body = uti.httpResponse(0, '用户登陆成功', {token: token})
+      ctx.response.body = uti.httpResponse(0, {token: token})
     })
    .catch((err) => {
-     ctx.response.body = uti.httpResponse(1, '用户登陆失败', err)
+     ctx.response.body = uti.httpResponse(2002, err)
    })
 
   return next()
@@ -81,25 +83,25 @@ router.post('/session', body(), async function (ctx, next) {
   yield next
 })
 
-// DELETE /api/session 用户退出登陆 销毁当前会话
+// DELETE /api/session 账号退出登陆 销毁当前会话
 router.del('/session/:sessionId', body(), function * (next) {
-  // 分离并取最后的/:sessionId  一般等于token，除非是管理员手动删除 用户的会话
+  // 分离并取最后的/:sessionId  一般等于token，除非是管理员手动删除 账号的会话
   let sessionId = this.request.url.split('/').pop()
-  // 从header中提取出token。只有所属用户或管理员的token才允许删除此会话
-  let token = this.request.header.token
+  // 从header中提取出token。只有所属账号或管理员的token才允许删除此会话
+  let token = this.cookies.get('token') || this.request.header.token
   console.log(token, sessionId)
   // 从缓存中删除此token
-  this.response.body = uti.httpResponse(0, '用户退出登陆')
+  this.response.body = uti.httpResponse(0)
   yield next
 })
 
 // DELETE /user/:name 账号注销 删除数据
 router.del('/user/:name', body(), function * (next) {
-  // 从header中提取出token  只有管理员与或属用户的token才能进行账号注销
+  // 从header中提取出token  只有管理员与或属账号的token才能进行账号注销
   let token = this.request.header.token
   let name = this.request.url.split('/').pop()// 分离并取最后的/:id
   // TODO:敏感操作，应该弹窗提示 是否确认这样做 或许甚至理当输入密码才能做。
-  this.response.body = uti.httpResponse(0, '账号注销成功')
+  this.response.body = uti.httpResponse(0)
   yield next
 })
 
--- src/utilities.js
+++ src/utilities.js
@@ -1,6 +1,7 @@
 const md5 = require('md5')
 const moment = require('moment')
 const _ = require('lodash')
+const errorCodeList = require('../config/error-code.js')
 
 let utilities = {
   /**
@@ -27,27 +28,15 @@ let utilities = {
    * http JSON回复格式
    *
    * @param {number} error_code 失败码 0-正常 非0为错误
-   * @param {string} message 携带信息
    * @param {any} data 携带数据
    * @returns {object} 自定义标准的http回复JSON格式
    *
    */
-  httpResponse (errorCode, message, data) {
-    // {
-    //   "error_code": 0,
-    //   "data": {
-    //     "uid": "1",
-    //     "username": "12154545",
-    //     "name": "吴系挂",
-    //     "groupid": 2 ,
-    //     "reg_time": "1436864169",
-    //     "last_login_time": "0",
-    //   }
-    // }
+  httpResponse (errorCode, data) {
     if (data instanceof Error) data = data.message// JSON.stringify(new Error('test'))会变成{}
     return JSON.stringify({
       error_code: errorCode,
-      message: message,
+      message: errorCodeList['' + errorCode][0] || '未知错误代码',
       data: data || {}
     })
   }
