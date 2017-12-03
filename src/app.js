d756c545a86474b7e2d174b5873d8541a109d978
生子当如哈士奇 <592364224@qq.com>
2017-12-03 10:36:56 +0800

    添加跨域请求功能

--- src/app.js
+++ src/app.js
@@ -3,16 +3,19 @@ const router = require('./router.js')
 const handleError = require('koa-handle-error')
 const logger = require('koa-logger')
 const log = require('./log.js')
-let api = require('./api.js')
+const api = require('./api.js')
 
+const PORT = require('../config/config.js').PORT
+const cors = require('@koa/cors')
 const app = new Koa()
+// app.use(handleError((err) => { log.error(err) }))  // 错误处理
 
-app.use(handleError((err) => { log.error(err) }))  // 错误处理
 app.use(logger())// 访问日志记录
 
+app.use(cors())  // 由于前后端分离，须支持跨域请求
 app.use(router.middleware())// 路由文件
 app.use(api.middleware())// API路由文件
 
-app.listen(3000, () => {
-  console.log('listening ' + 'http://localhost:3000')
+app.listen(PORT, () => {
+  console.log('listening ' + 'http://localhost:' + PORT)
 })
