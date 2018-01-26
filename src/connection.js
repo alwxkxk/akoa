d75234e8d320d1bdadadeffdc7a90c57a3b47186
生子当如哈士奇 <592364224@qq.com>
2018-01-26 11:25:55 +0800

    在socketio里加入systemStatus room逻辑

--- src/connection.js
+++ src/connection.js
@@ -16,21 +16,24 @@ setInterval(() => {
 // 引入socket.io
 exports.socketioInit = function socketioInit (server) {
   const io = require('socket.io')(server)
+  // 给systemStatus room里所有客户端 定时发送 系统状态
+  setInterval(() => {
+    io.to('systemStatus room').emit('systemStatus', systemSatusJSON)
+  }, systemInfoInterval * 1000)
+
   io.on('connection', function (socket) {
-    systemStatus(socket)
+    // 客户端 请求join systemStatus room
+    socket.on('join systemStatus room', () => {
+      console.log('client join systemStatus room.')
+      socket.join('systemStatus room')
+    })
+    // 客户端 请求leave systemStatus room
+    socket.on('leave systemStatus room', () => {
+      console.log('client leave systemStatus room.')
+      socket.leave('systemStatus room')
+    })
     socket.on('message', function (msg) {
       console.log('message:', msg)
     })
   })
-}
-
-/**
- * 给socket定时发送系统状态 //TODO；改用room来实现
- *
- * @param {any} socket
- */
-function systemStatus (socket) {
-  setInterval(() => {
-    socket.emit('systemStatus', systemSatusJSON)
-  }, systemInfoInterval * 1000)
 }
